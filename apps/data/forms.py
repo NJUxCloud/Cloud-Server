87feeb5517cc1e70f6425ae8cf09428045ad8156
151250206 <151250206@smail.nju.edu.cn>
2018-01-02 12:38:26 +0800

    <dev> : <add doc uploader> <delete forms.py in apps/data>

--- apps/data/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import forms
-
-
-class FileUploadForm(forms.Form):
-    """
-    处理文件上传的表单类
-    """
-    file = forms.FileField()
--- apps/data/urls.py
+++ apps/data/urls.py
@@ -1,9 +1,10 @@
 from django.urls import re_path
 
 from apps.data.views import code_view
+from apps.data.views import doc_view
 
 urlpatterns = [
     # 处理代码文件
     re_path(r'^code/$', code_view.CodeView.as_view()),
-
+    re_path(r'^doc/$', doc_view.DocView.as_view()),
 ]
