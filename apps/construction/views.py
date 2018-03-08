1fb0c3b09c6499c7c223b944e102ed4e4fc7dbf5
Green-Cherry <2423964104@qq.com>
2018-03-08 16:06:20 +0800

    fix:修改路径

--- apps/construction/views.py
+++ apps/construction/views.py
@@ -48,7 +48,7 @@ def getModelNameList(path):
     files = os.listdir(path)
     for f in files:
         if (os.path.isdir(path + '/' + f)):
-            if(os.path.exists(path + '/' + f+'result.txt')):
+            if(os.path.exists(path + '/' + f+'/result.txt')):
                 # 添加文件夹
                 dirList.append(f)
     return dirList
